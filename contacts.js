1a7cefc8be4af361b675092f3220af2a02cff73c
BartekJedliczka <bartekjedliczka@gmail.com>
2023-03-21 20:16:28 -0400

    update

--- contacts.js
+++ contacts.js
@@ -20,12 +20,21 @@ async function removeContact(contactId) {
   return newContacts;
 }
 
-async function addContact(name, email, phone, id) {
-  const contact = { name, email, phone, id };
-  const contacts = await listContacts();
-  contacts.push(contact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return contact;
+async function addContact(name, email, phone) {
+  const data = await fs.readFile(contactsPath);
+  const dataParse = JSON.parse(data);
+  const contactIndex = Number(dataParse[dataParse.length - 1].id) + 1;
+  const addData = [
+    ...dataParse,
+    {
+      id: `${contactIndex}`,
+      name,
+      email,
+      phone,
+    },
+  ];
+  await fs.writeFile(contactsPath, JSON.stringify(addData));
+  return addData;
 }
 
 module.exports = {
